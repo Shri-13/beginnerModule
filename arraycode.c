71fad81314a67e6c1be23314ab28d8d9a996098e
kashik-martialeagle <71945723+kashik-martialeagle@users.noreply.github.com>
2021-07-02 01:25:43 +0530

    Update arraycode.c

--- arraycode.c
+++ arraycode.c
@@ -3,16 +3,16 @@
 
 int main () {
 
-   int n[ 10 ]; /* n is an array of 10 integers */
+   int n[ 5 ]; /* n is an array of 10 integers */
    int i,j;
 
    /* initialize elements of array n to 0 */
-   for ( i = 0; i < 10; i++ ) {
-      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   for ( i = 0; i < 5; i++ ) {
+      n[ i ] = i + 50; /* set element at location i to i + 50 */
    }
 
    /* output each array element's value */
-   for (j = 0; j < 10; j++ ) {
+   for (j = 0; j < 5; j++ ) {
       printf("Element[%d] = %d\n", j, n[j] );
    }
 
